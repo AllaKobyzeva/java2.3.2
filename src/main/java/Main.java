a1b84475b160c44803c434aca54d802ce23cc3a0
Alla Kobyzeva <apkobyzeva@yandex.ru>
2021-05-30 18:18:05 +0300

    fixed #1 Исправлен код в классе Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
@@ -5,7 +7,10 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+   long bonus = service.calculate(amount, registered);
+
+    System.out.println(bonus);
+
   }
 }
 
